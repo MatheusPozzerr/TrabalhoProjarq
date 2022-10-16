856b2617dee2a957c5cfe325c92e9714179f7da6
giovanebmilani <giovanebmilani@hotmail.com>
2022-10-16 15:40:30 -0300

    muda filter para camada services

--- src/main/java/com/dev/trabProjarq/InterfacesAdaptadoras/Repositorios/RepositorioOcupacaoAerovia.java
+++ src/main/java/com/dev/trabProjarq/InterfacesAdaptadoras/Repositorios/RepositorioOcupacaoAerovia.java
@@ -2,14 +2,11 @@ package com.dev.trabProjarq.InterfacesAdaptadoras.Repositorios;
 
 import com.dev.trabProjarq.dominio.entities.OcupacaoAerovia;
 import com.dev.trabProjarq.dominio.services.IOcupacaoAeroviaRep;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class RepositorioOcupacaoAerovia implements IOcupacaoAeroviaRep {
@@ -21,12 +18,8 @@ public class RepositorioOcupacaoAerovia implements IOcupacaoAeroviaRep {
 	}
 
 	@Override
-	public List<OcupacaoAerovia> findOcupadasSlots(int aeroviaId, LocalDate data, List<Float> slotsHorarios) {
-		DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-		return ocupacaoAeroviaCrud.findAll().stream()
-			.filter(oa -> oa.aerovia.id == aeroviaId)
-			.filter(oa -> oa.data.equals(data))
-			.filter(oa -> slotsHorarios.contains((float) oa.slot_horario))
-			.collect(Collectors.toList());
+	public List<OcupacaoAerovia> findAll() {
+		return ocupacaoAeroviaCrud.findAll();
 	}
+
 }
--- src/main/java/com/dev/trabProjarq/dominio/services/IOcupacaoAeroviaRep.java
+++ src/main/java/com/dev/trabProjarq/dominio/services/IOcupacaoAeroviaRep.java
@@ -2,9 +2,8 @@ package com.dev.trabProjarq.dominio.services;
 
 import com.dev.trabProjarq.dominio.entities.OcupacaoAerovia;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface IOcupacaoAeroviaRep {
-    List<OcupacaoAerovia> findOcupadasSlots(int aeroviaId, LocalDate data, List<Float> slotsHorarios);
+    List<OcupacaoAerovia> findAll();
 }
