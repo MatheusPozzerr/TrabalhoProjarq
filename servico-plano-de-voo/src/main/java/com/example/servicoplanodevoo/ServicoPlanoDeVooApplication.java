acbd4b8a19ac54168a2cc7e9a8e455e9162b727b
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2022-11-26 15:30:04 -0300

    separando o monolito do micro servico de plano de voo

---
+++ servico-plano-de-voo/src/main/java/com/example/servicoplanodevoo/ServicoPlanoDeVooApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicoplanodevoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoPlanoDeVooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoPlanoDeVooApplication.class, args);
+	}
+
+}
---
+++ servico-plano-de-voo/src/test/java/com/example/servicoplanodevoo/ServicoPlanoDeVooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicoplanodevoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoPlanoDeVooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
